b7317edab9df8d8942d3d767bc89129aa99e8554
Astraeo <jhunhenrygamboacatiloc@gmail.com>
2026-04-24 01:30:05 +0800

    V10 of core js

--- core.js
+++ core.js
@@ -1,1 +1,24 @@
+let xp = +localStorage.xp || 0;
+let level = +localStorage.level || 1;
 
+let category = localStorage.category || "";
+let questBoard = JSON.parse(localStorage.questBoard || "[]");
+let activeQuest = "";
+
+function save(){
+  localStorage.xp = xp;
+  localStorage.level = level;
+  localStorage.category = category;
+  localStorage.questBoard = JSON.stringify(questBoard);
+}
+
+function updateLevel(){
+  let req = level * 100;
+
+  while(xp >= req){
+    xp -= req;
+    level++;
+    req = level * 100;
+    console.log("LEVEL UP");
+  }
+}
