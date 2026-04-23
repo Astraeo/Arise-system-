51bdb15bd1b5979b6d33eea946a32a5d0df580f5
Astraeo <jhunhenrygamboacatiloc@gmail.com>
2026-04-24 01:32:04 +0800

    V10 of rewards js

--- rewards.js
+++ rewards.js
@@ -1,1 +1,11 @@
+function reward(){
 
+  xp += 50;
+
+  if(category === "strength") console.log("STR +1");
+  if(category === "stamina") console.log("STA +1");
+  if(category === "agility") console.log("AGI +1");
+
+  updateLevel();
+  save();
+}
