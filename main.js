5a324a504fab5f634ee316848eedcdcadc5d61d8
Astraeo <jhunhenrygamboacatiloc@gmail.com>
2026-04-24 01:32:30 +0800

    V10 of main Js

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
+function initGame(){
+  generateBoard();
+  renderStats();
+}
 
+function renderStats(){
+  document.getElementById("stats").innerText =
+  "LEVEL: " + level + " | XP: " + xp;
+}
+
+function startQuest(i){
+
+  let q = questBoard[i];
+
+  activeQuest = q.t;
+  activeType = q.type;
+
+  if(q.type === "sensor"){
+    repCount = 0;
+    startSensor();
+  }
+
+  console.log("Quest started:", q.t);
+}
+
+function completeQuest(){
+
+  reward();
+
+  activeQuest = "";
+  generateBoard();
+  renderStats();
+}
