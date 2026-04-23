759672f627e6330323658476cf17399027e080c0
Astraeo <jhunhenrygamboacatiloc@gmail.com>
2026-04-24 01:31:33 +0800

    V10 of sensor js

--- sensor.js
+++ sensor.js
@@ -1,1 +1,29 @@
+let repCount = 0;
+let lastTime = 0;
+let activeType = "";
 
+function startSensor(){
+
+  window.addEventListener("devicemotion", e=>{
+
+    if(activeType !== "sensor") return;
+
+    let a = e.accelerationIncludingGravity;
+
+    let total = Math.abs(a.x) + Math.abs(a.y) + Math.abs(a.z);
+
+    let now = Date.now();
+
+    if(total > 25 && now - lastTime > 600){
+      repCount++;
+      lastTime = now;
+
+      console.log("REP:", repCount);
+    }
+
+    if(repCount >= 10){
+      window.removeEventListener("devicemotion", arguments.callee);
+      completeQuest();
+    }
+  });
+}
